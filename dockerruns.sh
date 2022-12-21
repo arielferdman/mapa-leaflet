1def56005cacf7ff08550c53aae71c7f20317fc2
root <root@vmi1018092.contaboserver.net>
2022-12-21 14:45:22 +0100

    fixed bug in dockerruns.sh

--- dockerruns.sh
+++ dockerruns.sh
@@ -1,5 +1,5 @@
 docker run -d --name python --network mapot-net -h python-backend python:alpine tail -f /dev/null
 
-docker run -d -v -p 8080:8080 --expose=5432 mapot-volume:/var/lib/postgresql/11/main arielfe/nominatim-israel
+docker run -d -p 8080:8080 --expose=5432 -v mapot-volume:/var/lib/postgresql/11/main arielfe/nominatim-israel
 
 docker run --name some-nginx -v /home/ubuntu/mapa-leaflet:/usr/share/nginx/html -d -p 80:80 nginx:alpine
